48032b9979dd92a87f66a6c0de30f1f9daa93424
nishantpatel11111 <nishantsatasiya27@gmail.com>
2025-07-16 19:48:46 +0530

    port binding error fixed

--- app.py
+++ app.py
@@ -86,4 +86,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment, default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
+
